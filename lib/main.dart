81bf30e210de8c50bfcbd883080fc2d1d0171a59
mikias <mikikebe84@gmail.com>
2024-11-06 21:32:19 +0300

    feat:the global theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,30 @@
 import "package:expenses_tracker/screens/home_screen.dart";
 import 'package:flutter/material.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+var kDarkColorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark, seedColor: Color.fromARGB(255, 5, 99, 125));
 void main() {
   runApp(
-    const MaterialApp(
-      home: HomePage(),
+    MaterialApp(
+      darkTheme: ThemeData().copyWith(
+          appBarTheme: const AppBarTheme().copyWith(
+              backgroundColor: kDarkColorScheme.primary,
+              foregroundColor: kDarkColorScheme.onPrimary)),
+      theme: ThemeData().copyWith(
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primary,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondary,
+        ),
+        textTheme: const TextTheme().copyWith(
+          titleLarge: TextStyle(fontSize: 15, color: kColorScheme.primary),
+        ),
+      ),
+      home: const HomePage(),
     ),
   );
 }
